d1598c6f2aed4d83a94015d1ca3d7f6b11192f80
rohan1233424 <155885949+rohan1233424@users.noreply.github.com>
2024-01-27 00:29:46 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -49,7 +49,7 @@ class Config(object):
 	FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
 	BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
 	LAZY_CHANNEL = int(os.environ.get('LAZY_CHANNEL','-1002040694942'))
-	LAZY_MODE = bool(os.environ.get("LAZY_MODE", True))
+	LAZY_MODE = bool(os.environ.get("LAZY_MODE", False))
 	LAZY_PIC = os.environ.get("LAZY_PIC","https://te.legra.ph/file/169d734e703d77c938ddf.jpg")
 	LP_BTN_MAIN_CH_USRNM = os.environ.get("LP_BTN_MAIN_CH_USRNM")
 	LP_CHANNEL_USRNM = os.environ.get("LP_CHANNEL_USRNM")
